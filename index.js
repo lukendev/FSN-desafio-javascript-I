0b52669854352d836e28e815fd2c2b04e1ce0e73
lukendev <lucas.azevedo2104@gmail.com>
2020-03-02 19:44:45 -0300

    Funcionalidades do sistema escolar finalizadas!

--- index.js
+++ index.js
@@ -1,5 +1,224 @@
 // Base a ser utilizada
-const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
+const alunosDaEscola = [{
+    nome: "Henrique",
+    notas: [],
+    cursos: [],
+    faltas: 5
+}, {
+    nome: "Edson",
+    notas: [],
+    cursos: [],
+    faltas: 2
+}, {
+    nome: "Bruno",
+    notas: [10, 9.8, 9.6],
+    cursos: [],
+    faltas: 0
+}, {
+    nome: "Guilherme",
+    notas: [10, 9.8, 9.6],
+    cursos: [{
+        nomeDoCurso: "Full Stack",
+        dataMatricula: new Date
+    }],
+    faltas: 0
+}, {
+    nome: "Carlos",
+    notas: [],
+    cursos: [],
+    faltas: 0
+}, {
+    nome: "Lucca",
+    notas: [10, 9.8, 9.6],
+    cursos: [{
+        nomeDoCurso: "UX",
+        dataMatricula: new Date
+    }],
+    faltas: 0
+}];
+
+let listaDeAlunos = alunosDaEscola
+
+console.log(" ")
+console.log(" ")
+console.log("*** Sistema Escolar ***")
+console.log(" ")
+console.log(" ")
 
 
 // implementação
+function adicionarAluno(nome) {
+    let novoAluno = {
+        nome: nome,
+        notas: [],
+        cursos: [],
+        faltas: 0
+    }
+    listaDeAlunos.push(novoAluno)
+    console.log("----------------------")
+    console.log(nome + " foi inserido na nossa lista de alunos!")
+    console.log("----------------------")
+    console.log(" ")
+    /*Essa função irá receber uma *string* que é nome do aluno a ser criado. 
+    E seguindo o modelo de aluno, o mesmo deverá ser inserido na lista de alunos.
+    A função deve devolver um feedback de sucesso, caso o aluno seja inserido corretamente.*/
+}
+
+function listarAlunos() {
+    console.log("----------------------")
+    console.log("Lista de alunos cadastrados no nosso sistema: ")
+    console.log(" ")
+    console.log(" ")
+    for (let i = 0; i < listaDeAlunos.length; i++) {
+        console.log("Nome: " + listaDeAlunos[i].nome)
+        if (listaDeAlunos[i].notas.length > 0) {
+            console.log("Notas: " + listaDeAlunos[i].notas.join(", "))
+        } else {
+            console.log("Notas: -/-")
+        }
+        if (listaDeAlunos[i].cursos.length > 0) {
+            console.log("*** Cursos matriculados: ")
+            for (let y = 0; y < listaDeAlunos[i].cursos.length; y++) {
+                let curso = listaDeAlunos[i].cursos[y].nomeDoCurso
+                let data = listaDeAlunos[i].cursos[y].dataMatricula
+                console.log("Curso " + (y+1) + ": " + curso)
+                console.log("Data de matrícula: " + data.getDay() + "/" + data.getMonth() + "/" + data.getFullYear())
+                console.log("-//-")
+            }
+        } else {
+            console.log("Não está matriculado em nenhum curso!")
+        }
+        console.log("Faltas: " + listaDeAlunos[i].faltas)
+        console.log("----------")
+        //let alunoListado = listaDeAlunos[i]
+        // for(let prop in alunoListado){
+        //     if(prop == nome){
+        //         console.log("nome: " + alunoListado[prop])
+        //     } else if(prop == notas){
+        //         console.log("As notas ")
+        //     }
+        // }
+    }
+    /*Com essa função o usuário poderá ver todos os alunos cadastrados atualmente no sistema. 
+    Vale dizer que As informações deverão ser exibidas em um formato amigável.*/
+    console.log("----------------------")
+    console.log(" ")
+}
+
+function buscarAluno(nome) {
+    let alunosEncontrados = 0
+    let ids = []
+    let retorno = []
+    for (let i = 0; i < listaDeAlunos.length; i++) {
+        if (listaDeAlunos[i].nome == nome) {
+            alunosEncontrados += 1
+            ids.push(i)
+        }
+    }
+    if (alunosEncontrados == 0) {
+        console.log("Não foi encontrado nenhum aluno com o nome " + nome + " em nosso sistema.")
+    } else {
+        for (let i = 0; i < ids.length; i++) {
+            console.log("O aluno " + nome + " foi encontrado no nosso sistema!")
+            retorno.push(listaDeAlunos[ids[i]])
+        }
+        return retorno[0]
+    }
+    /* Por meio dessa função, podemos pesquisar um aluno por nome na lista de aluno. Ela deverá exibir um feedback, tanto para quando encontrar o aluno, tanto quando não encontrar. E deverá devolver um aluno em seu retorno. */
+}
+
+function matricularAluno(aluno, curso) {
+    let achouAluno = false
+    let novaData = new Date();
+    for (let i = 0; i < listaDeAlunos.length; i++) {
+        if (aluno == listaDeAlunos[i]) {
+            listaDeAlunos[i].cursos.push({
+                nomeDoCurso: curso,
+                dataMatricula: novaData
+            })
+            console.log("O aluno " + aluno.nome + " foi matriculado com sucesso no curso: " + curso)
+            achouAluno = true
+            //console.log(aluno)
+        }
+    }
+    if (!achouAluno) {
+        console.log("O aluno não foi encontrado em nosso sistema.")
+    }
+    /* Essa funcionalidade irá permitir, cadastrar um aluno em um curso. 
+    Essa função só poderá ser executada em um aluno já devidamente cadastrado no sistema, e deverá armazenar a data atual no momento da matricula
+    Lembre-se de exibir o feedback para o usuário. */
+}
+
+function aplicarFalta(aluno) {
+    let confereAluno = false
+    for (let i = 0; i < listaDeAlunos.length; i++) {
+        if (aluno == listaDeAlunos[i]) {
+            listaDeAlunos[i].faltas += 1
+            console.log("Falta adicionada ao aluno " + aluno.nome + " com sucesso!")
+            confereAluno = true
+        }
+    }
+    if (!confereAluno) {
+        console.log("Aluno não encontrado no sistema.")
+    }
+    /*
+     Ao receber um aluno devidamente cadastrado em nossa lista. Você deverá incrementar uma falta ao aluno. Você deverá dar um feedback ao concluir a tarefa. Só poderá aplicar falta em aluno se o mesmo tiver matriculado em um curso.
+    */
+}
+
+function aplicarNota(aluno, nota) {
+    let confereAluno = false
+    for (let i = 0; i < listaDeAlunos.length; i++) {
+        if (aluno == listaDeAlunos[i]) {
+            aluno.notas.push(nota)
+            console.log("Nota adicionada ao aluno: " + aluno.nome)
+            confereAluno = true
+        }
+    }
+    if (!confereAluno) {
+        console.log("Aluno não encontrado no sistema.")
+    }
+    /*
+     Ao receber um aluno devidamente cadastrado em nossa lista. Você deverá adicionar uma nota ao aluno na sua lista de notas. Você deverá dar um feedback ao concluir a tarefa. Só poderá aplicar nota em aluno se o mesmo tiver matriculado em um curso.
+    */
+}
+
+function aprovarAluno(aluno) {
+    let confereAluno = false
+    for (let i = 0; i < listaDeAlunos.length; i++) {
+        if (aluno == listaDeAlunos[i]) {
+            confereAluno = true
+            if (listaDeAlunos[i].faltas <= 3 && listaDeAlunos[i].notas.length > 0) {
+                let totalNotas = 0;
+                for (let y = 0; y < listaDeAlunos[i].notas.length; y++) {
+                    totalNotas += listaDeAlunos[i].notas[y]
+                }
+                if (totalNotas / aluno.notas.length >= 7) {
+                    console.log("Aluno " + listaDeAlunos[i].nome + " aprovado!")
+                } else {
+                    console.log("Aluno " + listaDeAlunos[i].nome + " reprovado!")
+                }
+            } else if (listaDeAlunos[i].faltas <= 3 && listaDeAlunos[i].notas.length == 0) {
+                console.log("Aluno " + listaDeAlunos[i].nome + " ainda não possui notas!")
+            } else if (listaDeAlunos[i].faltas > 3) {
+                console.log("Aluno reprovado por faltas!")
+            }
+        }
+    }
+    if (!confereAluno) {
+        console.log("Aluno não encontrado no sistema!")
+    }
+    /* 
+    Ao receber um aluno devidamente cadastrado em nossa lista, deverá dizer se o mesmo está aprovado ou não. Os critérios de aprovação são: ter no máximo 3 faltas e média 7 em notas.
+    Só o aluno só poderá ser aprovado se o mesmo tiver matriculado em um curso.
+    */
+}
+
+adicionarAluno("Lucas")
+listarAlunos()
+buscarAluno("Jefferson")
+listarAlunos()
+matricularAluno(listaDeAlunos[0], "Full Stack")
+aplicarFalta(listaDeAlunos[3])
+aplicarNota(listaDeAlunos[2], 7)
+aprovarAluno(listaDeAlunos[2])
